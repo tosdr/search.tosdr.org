027cd8710b285e7191d916338f2197c73611eb70
Justin Back <justin@tosdr.org>
2021-06-18 02:28:50 +0200

    Plugins now have translations too

--- utils/update_base_lang.sh
+++ utils/update_base_lang.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 pybabel extract ../. -o ../searx/translations/source/LC_MESSAGES/messages.po -F ../babel.cfg
-pybabel compile -d ../searx/translations
+#pybabel compile -d ../searx/translations
